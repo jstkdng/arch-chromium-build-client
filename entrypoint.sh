d62336894ca9f3a602062c7fd144a8eeb94ccb45
jk <jk@vin.ovh>
2020-06-11 09:45:34 -0500

    remove variables

--- entrypoint.sh
+++ entrypoint.sh
@@ -19,7 +19,6 @@ echo "Starting build..." > logfile
 go run .github/workflows/wait_workers.go |& tee -a logfile
 
 # start build
-export DISTCC_VERBOSE=1
 export DISTCC_HOSTS="--randomize 10.200.200.102/4,cpp,lzo 10.200.200.103/4,cpp,lzo 10.200.200.104/4,cpp,lzo 10.200.200.105/4,cpp,lzo"
 pump makepkg --nodeps |& tee -a logfile
 
