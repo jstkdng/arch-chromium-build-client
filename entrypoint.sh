b4d4b6fccb3786b3b2a9cea14d041ac859496eba
jk <jk@vin.ovh>
2020-06-11 09:06:27 -0500

    update distcc hosts

--- entrypoint.sh
+++ entrypoint.sh
@@ -21,6 +21,7 @@ go run .github/workflows/wait_workers.go |& tee -a logfile
 # start build
 export DISTCC_VERBOSE=1
 export DISTCC_DIR=/tmp/distcc
+export DISTCC_HOSTS="--randomize 10.200.200.102/4,cpp,lzo 10.200.200.103/4,cpp,lzo 10.200.200.104/4,cpp,lzo 10.200.200.105/4,cpp,lzo"
 pump makepkg --nodeps |& tee -a logfile
 
 # terminate workers
